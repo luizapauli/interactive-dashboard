ce522f0e9519260eb3847b1d5989d1dc15cffdb3
Luiza Pauli <luizapauli.c@gmail.com>
2026-03-16 15:15:16 -0300

    :sparkles: feat: graphics 2, 3, 4

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ if not df_filtered.empty:
     total_entries = df_filtered.shape[0]
     most_common_role = df_filtered['Role'].mode()[0]
 else:
-    avg_salary, max_salary, total_entries, most_common_job = 0,0,0,"N/A"
+    avg_salary, max_salary, total_entries, most_common_role = 0,0,0,"N/A"
 
 col1, col2, col3, col4 = st.columns(4, gap="small")
 col1.metric("Average Salary", f"${avg_salary:,.2f}")
@@ -80,8 +80,66 @@ with col_graph1:
             y = 'Role',
             orientation='h',
             title="Top 10 Roles by Average Salary",
-            labels={"USD": "Annual Average Salary (USD)", "Role": "Job Role"}
+            labels={"USD": "Annual Average Salary (USD)", "Role": "Job Role"},
+            color_discrete_sequence=['#29B09D']
         )
         st.plotly_chart(graph_roles, use_container_width=True)
     else:
         st.info("No data available for the selected filters.")
+
+with col_graph2:
+    if not df_filtered.empty:
+        salary_distribution = px.histogram(
+            df_filtered,
+            x='USD',
+            nbins=30,
+            title="Annual Salary Distribution",
+            labels={"USD": "Annual Salary (USD)", 'count': ''},
+            color_discrete_sequence=["#57CDBD"]
+        )
+        salary_distribution.update_layout(title_x=0.1)
+        st.plotly_chart(salary_distribution, use_container_width=True)
+    else:
+        st.info("No data available for the selected filters.")
+
+col_graph3, col_graph4 = st.columns(2, gap="medium")
+
+with col_graph3:
+    if not df_filtered.empty:
+        remote_count = df_filtered['Remote'].value_counts().reset_index()
+        remote_count.columns = ['index', 'Remote']
+        graph_remote = px.pie(
+            remote_count,
+            names='index',
+            values='Remote',
+            title="Remote vs On-site Work Distribution",
+            labels={"index": "Work Type", "Remote": "Count"},
+            color_discrete_sequence=['#29B09D', "#2FB8BF", '#83C9FF']
+        )
+        graph_remote.update_traces(textposition='inside', textinfo='percent+label')
+        graph_remote.update_layout(title_x=0.1)
+        st.plotly_chart(graph_remote, use_container_width=True)
+    else:
+        st.info("No data available for the selected filters.")
+
+with col_graph4:
+    if not df_filtered.empty:
+        df_ds = df_filtered[df_filtered['Role'] == 'Data Scientist']
+        medium_ds_country = df_ds.groupby('Country_ISO3')['USD'].mean().reset_index()
+        graph_countries = px.choropleth(
+            medium_ds_country,
+            locations= 'Country_ISO3',
+            color= 'USD',
+            color_continuous_scale= 'viridis',
+            title="Average Salary for Data Scientists by Country",
+            labels={"USD": "Average Salary (USD)", "Country_ISO3": "Country"}
+        )
+        graph_countries.update_layout(title_x=0.1)
+        st.plotly_chart(graph_countries, use_container_width=True)
+    else:
+        st.info("No data available for the selected filters.")
+
+st.divider()
+    
+st.subheader("Detailed Data View")
+st.dataframe(df_filtered)
